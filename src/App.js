139a956cc0e40618b72c3c7ec3e8def9c6685c0f
inventor++ <zzindasebali@gmail.com>
2023-05-22 13:59:41 +0300

    Functioning Product listing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./components/ProductListing";
+import Header from "./components/Header";
+import "./App.css";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <h1>invented by mikaili, inc</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,19 +1,13 @@
 import { ActionTypes } from "../constants/ActionTypes";
 
 const initialState = {
-    products: [
-        {
-            id: 1,
-            title: "Nike Shoes 01",
-            category: "Shoes",
-        },
-    ]
+    products: []
 }
 
 export const productReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
-            return state;
+            return { ...state, products: payload };
         default:
             return state;
     }
