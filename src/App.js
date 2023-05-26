f2872c992cb89d10960f29a69078e5e4610c68b8
inventor++ <zzindasebali@gmail.com>
2023-05-26 14:40:05 +0300

    inventor++ restarted the project.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProductListing from "./components/ProductListing";
-import Header from "./components/Header";
-import "./App.css";
-import ProductDetails from "./components/ProductDetails";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" exact component={ProductListing} />
-          <Route path="/product/:productId" component={ProductDetails} />
-          <Route>404 Not Found!</Route>
-        </Routes>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
