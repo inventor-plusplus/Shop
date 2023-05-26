451320fc81a540a1a3c1386e37b1d3412e430f5f
inventor++ <zzindasebali@gmail.com>
2023-05-26 15:58:13 +0300

    Completed the src/App.js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import ProductsListing from './views/ProductsListing'
+import ProductDetails from './views/ProductDetails'
 
 function App() {
   return (
     <div className="App">
-      invented by mikaili, inc
-     </div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductsListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found.</Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
